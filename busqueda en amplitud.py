48690296c72c234936c39e403ea67da4dbf12b90
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2023-03-13 20:57:22 -0600

    Update busqueda en amplitud.py

--- busqueda en amplitud.py
+++ busqueda en amplitud.py
@@ -1,55 +1,58 @@
 from collections import deque
 
-def bfs_grafo(grafo, inicio, objetivo):
-    """
-    Realiza una búsqueda en amplitud para encontrar la ruta más corta entre dos nodos en un grafo.
+# Definir el grafo
+grafo = {
+    'A': ['B', 'D'],
+    'B': ['A', 'C', 'E'],
+    'C': ['B', 'F'],
+    'D': ['A', 'E', 'G'],
+    'E': ['B', 'D', 'F', 'H'],
+    'F': ['C', 'E', 'I'],
+    'G': ['D', 'H', 'J'],
+    'H': ['E', 'G', 'I', 'K'],
+    'I': ['F', 'H', 'J', 'L'],
+    'J': ['G', 'I', 'K', 'M'],
+    'K': ['H', 'J', 'L'],
+    'L': ['I', 'K', 'M'],
+    'M': ['J', 'L']
+}
 
-    :param grafo: el grafo representado como un diccionario donde las claves son los nodos y los valores son las listas de nodos adyacentes
-    :param inicio: el nodo inicial
-    :param objetivo: el nodo objetivo
-    :return: una lista que representa la ruta más corta desde el nodo inicial hasta el nodo objetivo, o None si no se encuentra una ruta
-    """
-    queue = deque()
-    queue.append(inicio)
-
-    # Creamos un diccionario para almacenar el nodo anterior de cada nodo en el camino desde el nodo inicial
-    parents = {inicio: None}
-
-    # Creamos un diccionario para almacenar la distancia desde el nodo inicial hasta cada nodo visitado
-    distance = {inicio: 0}
-
-    while queue:
-        current = queue.popleft()
-
-        if current == objetivo:
-            path = [objetivo]
-            while parents[path[-1]] is not None:
-                path.append(parents[path[-1]])
-            return path[::-1]
-
-        for vecino in grafo[current]:
-            if vecino not in parents:
-                parents[vecino] = current
-                distance[vecino] = distance[current] + 1
-                queue.append(vecino)
-
+# Definir la función de búsqueda en amplitud
+def busqueda_amplitud(grafo, inicio, objetivo):
+    # Inicializar la cola con el nodo de inicio
+    cola = deque([inicio])
+    
+    # Inicializar el conjunto de nodos visitados y el diccionario de padres
+    visitados = set()
+    padres = {inicio: None}
+    
+    # Iterar hasta que se encuentre el objetivo o se agote la cola
+    while cola:
+        # Sacar el siguiente nodo de la cola y marcarlo como visitado
+        nodo = cola.popleft()
+        visitados.add(nodo)
+        
+        # Si se encontró el objetivo, reconstruir el camino y regresarlo
+        if nodo == objetivo:
+            camino = [nodo]
+            padre = padres[nodo]
+            while padre is not None:
+                camino.append(padre)
+                padre = padres[padre]
+            camino.reverse()
+            return camino
+        
+        # Encontrar todos los vecinos no visitados del nodo actual y agregarlos a la cola
+        for vecino in grafo[nodo]:
+            if vecino not in visitados:
+                cola.append(vecino)
+                padres[vecino] = nodo
+    
+    # Si no se encontró el objetivo, regresar None
     return None
 
 # Ejemplo de uso
-grafo = {
-    'Calle A': ['Calle B', 'Calle C'],
-    'Calle B': ['Calle D', 'Calle E'],
-    'Calle C': ['Calle F'],
-    'Calle D': [],
-    'Calle E': ['Calle F'],
-    'Calle F': []
-}
-
-inicio = 'Calle B'
-objetivo = 'Calle F'
-ruta_mas_corta = bfs_grafo(grafo, inicio, objetivo)
-
-if ruta_mas_corta:
-    print(f"La ruta más corta desde {inicio} hasta {objetivo} es: {ruta_mas_corta}")
-else:
-    print(f"No se encontró una ruta desde {inicio} hasta {objetivo}")
+inicio = 'A'
+objetivo = 'M'
+camino = busqueda_amplitud(grafo, inicio, objetivo)
+print(camino)
